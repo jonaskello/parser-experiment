197ae29a43fd9921f1e768356f651caea61c822a
Jonas Kello <jonas.kello@gmail.com>
2024-04-11 00:37:32 +0200

    2

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -76,3 +76,74 @@ export function tokenize(input): ReadonlyArray<Token> {
     throw new SyntaxError(`Unexpected token: "${inputSlice[0]}"`);
   }
 }
+
+const isNumeric = (char: string) => !isNaN(parseInt(char));
+const isLetter = (char: string) => /[a-z]/i.test(char);
+
+export function tokenize2(input: string): ReadonlyArray<Token> {
+  let cursor = 0;
+  const tokens: Array<Token> = [];
+  while (cursor < input.length) {
+    if (isNumeric(input[cursor])) {
+      let number = input[cursor++];
+      while (cursor < input.length && isNumeric(input[cursor])) {
+        number += input[cursor++];
+      }
+      tokens.push({ type: TokenTypes.NUMBER, value: number });
+    } else if (isLetter(input[cursor])) {
+      let identifier = input[cursor++];
+      while (cursor < input.length && isNumeric(input[cursor])) {
+        identifier += input[cursor++];
+      }
+      tokens.push({ type: TokenTypes.IDENTIFIER, value: identifier });
+    } else {
+      switch (input[cursor]) {
+        case "+":
+          tokens.push({ type: TokenTypes.ADDITION, value: input[cursor] });
+          cursor++;
+          break;
+        case "-":
+          tokens.push({ type: TokenTypes.SUBTRACTION, value: input[cursor] });
+          cursor++;
+          break;
+        case "*":
+          tokens.push({
+            type: TokenTypes.MULTIPLICATION,
+            value: input[cursor],
+          });
+          cursor++;
+          break;
+        case "/":
+          tokens.push({ type: TokenTypes.DIVISION, value: input[cursor] });
+          cursor++;
+          break;
+        case "^":
+          tokens.push({
+            type: TokenTypes.EXPONENTIATION,
+            value: input[cursor],
+          });
+          cursor++;
+          break;
+        case "(":
+          tokens.push({
+            type: TokenTypes.PARENTHESIS_LEFT,
+            value: input[cursor],
+          });
+          cursor++;
+          break;
+        case ")":
+          tokens.push({
+            type: TokenTypes.PARENTHESIS_RIGHT,
+            value: input[cursor],
+          });
+          cursor++;
+        case " ":
+          cursor++;
+          break;
+        default:
+          throw new SyntaxError(`Unexpected token: "${input[cursor]}"`);
+      }
+    }
+  }
+  return tokens;
+}
