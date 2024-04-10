11d02172f9d825ffe622b70128733d5dc601ffe0
Jonas Kello <jonas.kello@gmail.com>
2024-04-11 00:38:04 +0200

    Cleanuo

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -15,68 +15,6 @@ export const TokenTypes = {
   PARENTHESIS_RIGHT: ")",
 };
 
-const TokenSpec: ReadonlyArray<[RegExp, string | null]> = [
-  [/^\s+/, null],
-  [/^(?:\d+(?:\.\d*)?|\.\d+)/, TokenTypes.NUMBER],
-  [/^[a-z]+/, TokenTypes.IDENTIFIER],
-  [/^\+/, TokenTypes.ADDITION],
-  [/^\-/, TokenTypes.SUBTRACTION],
-  [/^\*/, TokenTypes.MULTIPLICATION],
-  [/^\//, TokenTypes.DIVISION],
-  [/^\^/, TokenTypes.EXPONENTIATION],
-  [/^\(/, TokenTypes.PARENTHESIS_LEFT],
-  [/^\)/, TokenTypes.PARENTHESIS_RIGHT],
-];
-
-export function tokenize(input): ReadonlyArray<Token> {
-  let cursor = 0;
-
-  const tokens: Array<Token> = [];
-  let t = getNextToken();
-  while (t) {
-    tokens.push(t);
-    t = getNextToken();
-  }
-  return tokens;
-
-  function match(regex, inputSlice) {
-    const matched = regex.exec(inputSlice);
-    if (matched === null) {
-      return null;
-    }
-
-    cursor += matched[0].length;
-    return matched[0];
-  }
-
-  function getNextToken(): Token | null {
-    if (!(cursor < input.length)) {
-      return null;
-    }
-
-    const inputSlice = input.slice(cursor);
-
-    for (let [regex, type] of TokenSpec) {
-      const tokenValue = match(regex, inputSlice);
-
-      if (tokenValue === null) {
-        continue;
-      }
-
-      if (type === null) {
-        return getNextToken();
-      }
-
-      return {
-        type,
-        value: tokenValue,
-      };
-    }
-
-    throw new SyntaxError(`Unexpected token: "${inputSlice[0]}"`);
-  }
-}
-
 const isNumeric = (char: string) => !isNaN(parseInt(char));
 const isLetter = (char: string) => /[a-z]/i.test(char);
 
