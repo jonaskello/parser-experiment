8e49f800830ef976547eaf4cf89de5fe4f759419
Jonas Kello <jonas.kello@gmail.com>
2024-04-11 14:17:06 +0200

    Add

--- src/ast.ts
+++ src/ast.ts
@@ -13,3 +13,37 @@ export type ValueRangeExpr = { type: "ValueRangeExpr"; min: AstNode; max: AstNod
 export type UnaryExpression = { type: "UnaryExpression"; value: Identifier | Numeric };
 export type Identifier = { type: "Identifier"; name: string; value: string };
 export type Numeric = { type: "Numeric"; value: number };
+
+/*
+type ParserCallbacks = {
+  createValueExpr(unparsed: string): Ast.ValueExpr;
+  createNullExpr(): Ast.NullExpr;
+  createIdentifierExpr(identToken: string): Ast.IdentifierExpr;
+  createValueRangeExpr(v1: Ast.PropertyValueExpr, v2: Ast.PropertyValueExpr): Ast.ValueRangeExpr;
+  createEqualsExpr(
+    leftValue: Ast.PropertyValueExpr,
+    operationType: Ast.EqualsOperationType,
+    rightValueRanges: ReadonlyArray<Ast.ValueRangeExpr>
+  ): Ast.EqualsExpr;
+  createComparisonExpr(
+    leftValue: Ast.PropertyValueExpr,
+    operationType: Ast.ComparisonOperationType,
+    rightValue: Ast.PropertyValueExpr
+  ): Ast.ComparisonExpr;
+  createAndExpr(children: ReadonlyArray<Ast.BooleanExpr>): Ast.AndExpr;
+  createOrExpr(children: ReadonlyArray<Ast.BooleanExpr>): Ast.OrExpr;
+  createAddExpr(
+    left: Ast.PropertyValueExpr,
+    operationType: Ast.AddExprOperationType,
+    right: Ast.PropertyValueExpr
+  ): Ast.AddExpr;
+  createMulExpr(
+    left: Ast.PropertyValueExpr,
+    operationType: Ast.MulExprOperationType,
+    right: Ast.PropertyValueExpr
+  ): Ast.MulExpr;
+  createUnaryExpr(operationType: Ast.UnaryExprOperationType, value: Ast.PropertyValueExpr): Ast.UnaryExpr;
+};
+
+
+*/
