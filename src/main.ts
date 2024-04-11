e61bc914e5bfa40a6c2af90845ad801803459727
Jonas Kello <jonas.kello@gmail.com>
2024-04-11 15:52:57 +0200

    Values

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { PropertyValue } from "./ast";
 import { evaluateAst } from "./eval";
 import { parse } from "./parser";
 import { printAst } from "./print-ast";
@@ -6,16 +7,17 @@ import { TokenizeState, getNextToken } from "./tokenizer";
 main();
 
 function main() {
-  const input = "size=1+2 & a!=3,1 | foo>55 | foo=1~5,10";
+  const propertyFilter = "size=1+2 & a!=3,1 | foo>55 | foo=1~5,10";
+  const propertyValues: Record<string, PropertyValue> = { size: { type: "integer", value: 12 } };
   // const input = "size=1~2";
 
-  printAllTokens(input);
+  printAllTokens(propertyFilter);
 
-  const ast = parse(input);
+  const ast = parse(propertyFilter);
   console.dir(ast, { depth: null }); // log nested objects
 
   // printAst(ast);
-  const result = evaluateAst(ast, {}, true);
+  const result = evaluateAst(ast, propertyValues, true);
   console.log("result", result);
 }
 
